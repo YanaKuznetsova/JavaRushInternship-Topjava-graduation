29660bb01f5baa56835faeb4c5464097d2de8ea6
Yana Kuznetsova <kuznetsova.jana@gmail.com>
2019-03-20 18:09:40 +0300

    implement UserService

---
+++ src/main/java/ru/kuznetsova/topjava/graduation/util/ValidationUtil.java
@@ -0,0 +1,54 @@
+package ru.kuznetsova.topjava.graduation.util;
+
+import javassist.NotFoundException;
+import ru.kuznetsova.topjava.graduation.model.AbstractEntity;
+
+public class ValidationUtil {
+
+    public static <T> T checkNotFoundWithId(T object, int id) throws NotFoundException {
+        return checkNotFound(object, "id=" + id);
+    }
+
+    private ValidationUtil() {
+    }
+
+    public static void checkNotFoundWithId(boolean found, int id) throws NotFoundException {
+        checkNotFound(found, "id=" + id);
+    }
+
+    public static <T> T checkNotFound(T object, String msg) throws NotFoundException {
+        checkNotFound(object != null, msg);
+        return object;
+    }
+
+    public static void checkNotFound(boolean found, String msg) throws NotFoundException {
+        if (!found) {
+            throw new NotFoundException("Not found entity with " + msg);
+        }
+    }
+
+    public static void checkNew(AbstractEntity entity) {
+        if (!entity.isNew()) {
+            throw new IllegalArgumentException(entity + " must be new (id=null)");
+        }
+    }
+
+    public static void assureIdConsistent(AbstractEntity entity, int id) {
+        if (entity.isNew()) {
+            entity.setId(id);
+        } else if (entity.getId() != id) {
+            throw new IllegalArgumentException(entity + " must be with id=" + id);
+        }
+    }
+
+    public static Throwable getRootCause(Throwable t) {
+        Throwable result = t;
+        Throwable cause;
+
+        while (null != (cause = result.getCause()) && (result != cause)) {
+            result = cause;
+        }
+        return result;
+    }
+
+}
