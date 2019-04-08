b36eceb0a368e1b6d6e776bfef816cc8cb2586a0
Yana Kuznetsova <kuznetsova.jana@gmail.com>
2019-04-08 20:05:34 +0300

    add namedQuery: UserRepository.delete

--- src/main/java/ru/kuznetsova/topjava/graduation/repository/CrudUserRepository.java
+++ src/main/java/ru/kuznetsova/topjava/graduation/repository/CrudUserRepository.java
@@ -3,6 +3,7 @@ package ru.kuznetsova.topjava.graduation.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 import ru.kuznetsova.topjava.graduation.model.User;
 
@@ -18,7 +19,8 @@ public interface CrudUserRepository extends JpaRepository<User, Integer> {
 
     @Transactional
     @Modifying
-    int deleteById(int id);
+    @Query(name = User.DELETE)
+    int deleteById(@Param("id") int id);
 
     @Override
     Optional<User> findById(Integer id);
