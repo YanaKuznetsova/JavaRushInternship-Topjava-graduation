c5c498decb43b07cf347ac934f270b8d190ce0fe
Yana Kuznetsova <kuznetsova.jana@gmail.com>
2019-04-24 18:59:16 +0300

    add user rest controllers tests

---
+++ src/test/java/ru/kuznetsova/topjava/lunchVotingSystem/web/admin/AdminUserRestControllerTest.java
@@ -0,0 +1,83 @@
+package ru.kuznetsova.topjava.lunchVotingSystem.web.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+import ru.kuznetsova.topjava.lunchVotingSystem.TestUtil;
+import ru.kuznetsova.topjava.lunchVotingSystem.model.Role;
+import ru.kuznetsova.topjava.lunchVotingSystem.model.User;
+import ru.kuznetsova.topjava.lunchVotingSystem.web.AbstractControllerTest;
+import ru.kuznetsova.topjava.lunchVotingSystem.web.json.JsonUtil;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.kuznetsova.topjava.lunchVotingSystem.TestUtil.readFromJsonResultActions;
+import static ru.kuznetsova.topjava.lunchVotingSystem.UserTestData.*;
+
+class AdminUserRestControllerTest extends AbstractControllerTest {
+
+    private static final String REST_URL = AdminUserRestController.REST_URL + '/';
+
+    @Test
+    void testGet() throws Exception {
+        mockMvc.perform(get(REST_URL + ADMIN_ID))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(getUserMatcher(ADMIN));
+    }
+
+    @Test
+    void testGetByEmail() throws Exception {
+        mockMvc.perform(get(REST_URL + "by?email=" + USER_1.getEmail()))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(getUserMatcher(USER_1));
+    }
+
+    @Test
+    void testDelete() throws Exception {
+        mockMvc.perform(delete(REST_URL + USER_ID))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+        assertMatchUsers(userService.getAll(), ADMIN, USER_2, USER_3, USER_4, USER_5);
+    }
+
+    @Test
+    void testUpdate() throws Exception {
+        User updated = new User(USER_1);
+        updated.setName("UpdatedName");
+        mockMvc.perform(put(REST_URL + USER_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(updated)))
+                .andExpect(status().isNoContent());
+
+        assertMatchUsers(userService.get(USER_ID), updated);
+    }
+
+    @Test
+    void testCreate() throws Exception {
+        User expected = new User(null, "New", "new@gmail.com", "newPass", Role.ROLE_USER);
+        ResultActions action = mockMvc.perform(post(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(expected)))
+                .andExpect(status().isCreated());
+
+        User returned = readFromJsonResultActions(action, User.class);
+        expected.setId(returned.getId());
+
+        assertMatchUsers(returned, expected);
+        assertMatchUsers(userService.getAll(), ADMIN, expected, USER_1, USER_2, USER_3, USER_4, USER_5);
+    }
+
+    @Test
+    void testGetAll() throws Exception {
+        TestUtil.print(mockMvc.perform(get(REST_URL))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(getUserMatcher(ADMIN, USER_1, USER_2, USER_3, USER_4, USER_5)));
+    }
+
+}
---
+++ src/test/java/ru/kuznetsova/topjava/lunchVotingSystem/web/user/UserProfileRestControllerTest.java
@@ -0,0 +1,48 @@
+package ru.kuznetsova.topjava.lunchVotingSystem.web.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import ru.kuznetsova.topjava.lunchVotingSystem.TestUtil;
+import ru.kuznetsova.topjava.lunchVotingSystem.model.Role;
+import ru.kuznetsova.topjava.lunchVotingSystem.model.User;
+import ru.kuznetsova.topjava.lunchVotingSystem.web.AbstractControllerTest;
+import ru.kuznetsova.topjava.lunchVotingSystem.web.json.JsonUtil;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.kuznetsova.topjava.lunchVotingSystem.UserTestData.*;
+import static ru.kuznetsova.topjava.lunchVotingSystem.web.user.UserProfileRestController.REST_URL;
+
+class UserProfileRestControllerTest extends AbstractControllerTest {
+
+    @Test
+    void testGet() throws Exception {
+        TestUtil.print(
+                mockMvc.perform(get(REST_URL))
+                        .andExpect(status().isOk())
+                        .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                        .andExpect(getUserMatcher(USER_1))
+        );
+    }
+
+    @Test
+    void testDelete() throws Exception {
+        mockMvc.perform(delete(REST_URL))
+                .andExpect(status().isNoContent());
+        assertMatchUsers(userService.getAll(), ADMIN, USER_2, USER_3, USER_4, USER_5);
+    }
+
+    @Test
+    void testUpdate() throws Exception {
+        User updated = new User(USER_ID, "newName", "newemail@ya.ru", "newPassword", Role.ROLE_USER);
+        mockMvc.perform(put(REST_URL).contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(updated)))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+
+        assertMatchUsers(userService.getByEmail("newemail@ya.ru"), updated);
+    }
+
+}
