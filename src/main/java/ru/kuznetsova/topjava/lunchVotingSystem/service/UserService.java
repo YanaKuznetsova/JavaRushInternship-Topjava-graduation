17ebca82b98570eb69f79c3d9171022fa45ee44a
Yana Kuznetsova <kuznetsova.jana@gmail.com>
2019-04-26 22:59:52 +0300

    minor: rename

--- src/main/java/ru/kuznetsova/topjava/lunchVotingSystem/service/UserService.java
+++ src/main/java/ru/kuznetsova/topjava/lunchVotingSystem/service/UserService.java
@@ -17,42 +17,42 @@ import static ru.kuznetsova.topjava.lunchVotingSystem.util.ValidationUtil.checkN
 @Service
 public class UserService {
 
-    private final UserRepository repository;
+    private final UserRepository userRepository;
 
     @Autowired
     public UserService(UserRepository repository) {
-        this.repository = repository;
+        this.userRepository = repository;
     }
 
     @CacheEvict(value = "users", allEntries = true)
     public User create(User user) {
         Assert.notNull(user, "user must not be null");
-        return repository.save(user);
+        return userRepository.save(user);
     }
 
     @CacheEvict(value = "users", allEntries = true)
     public void delete(int id) throws NotFoundException {
-        checkNotFoundWithId(repository.delete(id), id);
+        checkNotFoundWithId(userRepository.delete(id), id);
     }
 
     public User get(int id) throws NotFoundException {
-        return checkNotFoundWithId(repository.get(id), id);
+        return checkNotFoundWithId(userRepository.get(id), id);
     }
 
     public User getByEmail(String email) throws NotFoundException {
         Assert.notNull(email, "email must not be null");
-        return checkNotFound(repository.getByEmail(email), "email=" + email);
+        return checkNotFound(userRepository.getByEmail(email), "email=" + email);
     }
 
     @Cacheable("users")
     public List<User> getAll() {
-        return repository.getAll();
+        return userRepository.getAll();
     }
 
     @CacheEvict(value = "users", allEntries = true)
     public void update(User user) throws NotFoundException {
         Assert.notNull(user, "user must not be null");
-        checkNotFoundWithId(repository.save(user), user.getId());
+        checkNotFoundWithId(userRepository.save(user), user.getId());
     }
 
 }
