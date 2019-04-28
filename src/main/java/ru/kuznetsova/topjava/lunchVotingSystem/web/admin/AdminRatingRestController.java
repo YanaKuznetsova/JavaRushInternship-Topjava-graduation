013248b59f99e3f6e13e87af870f50dd5adb09a8
Yana Kuznetsova <kuznetsova.jana@gmail.com>
2019-04-28 12:10:11 +0300

    add AdminRatingRestController

---
+++ src/main/java/ru/kuznetsova/topjava/lunchVotingSystem/web/admin/AdminRatingRestController.java
@@ -0,0 +1,76 @@
+package ru.kuznetsova.topjava.lunchVotingSystem.web.admin;
+
+import javassist.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import ru.kuznetsova.topjava.lunchVotingSystem.model.Rating;
+import ru.kuznetsova.topjava.lunchVotingSystem.model.Vote;
+import ru.kuznetsova.topjava.lunchVotingSystem.service.RatingService;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping(AdminRatingRestController.REST_URL)
+public class AdminRatingRestController {
+
+    static final String REST_URL = "/rest/admin/rating";
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private RatingService ratingService;
+
+    @GetMapping(value = "/all/{date}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Rating> getRatingForDate(@PathVariable("date") String date) throws NotFoundException {
+        log.info("get rating for date {}", date);
+        LocalDate localDate = LocalDate.parse(date);
+        return ratingService.getRatingForDate(localDate);
+    }
+
+    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Vote> getAllVotes() {
+        log.info("get all votes");
+        return ratingService.getAllVotes();
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Rating> getRatingForRestaurantName(@PathVariable("id") int id) throws NotFoundException {
+        log.info("get rating for restaurant name with id {}", id);
+        return ratingService.getRatingForRestaurantName(id);
+    }
+
+    @GetMapping(value = "/{id}/{date}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Rating getRatingForRestaurantForDate(@PathVariable("id") int id, @PathVariable("date") String date)
+            throws NotFoundException {
+        log.info("get rating for restaurant with id {} for date {}", id, date);
+        LocalDate localDate = LocalDate.parse(date);
+        return ratingService.getRatingForRestaurantForDate(id, localDate);
+    }
+
+    @GetMapping(value = "/user/{id}/{date}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Vote getVoteForUserForDate(@PathVariable("id") int id, @PathVariable("date") String date) {
+        log.info("get vote for user with id {} for date {}", id, date);
+        LocalDate localDate = LocalDate.parse(date);
+        return ratingService.getVoteForUserForDate(id, localDate);
+    }
+
+    @DeleteMapping()
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deleteOldVotes() {
+        log.info("delete old votes");
+        ratingService.deleteOldVotes();
+    }
+
+    @DeleteMapping("/{date}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deleteOldVotesForDate(@PathVariable("date") String date) {
+        log.info("delete old votes for date {}", date);
+        LocalDate localDate = LocalDate.parse(date);
+        ratingService.deleteOldVotesForDate(localDate);
+    }
+
+}
---
+++ src/test/java/ru/kuznetsova/topjava/lunchVotingSystem/web/admin/AdminRatingRestControllerTest.java
@@ -0,0 +1,96 @@
+package ru.kuznetsova.topjava.lunchVotingSystem.web.admin;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import ru.kuznetsova.topjava.lunchVotingSystem.TestUtil;
+import ru.kuznetsova.topjava.lunchVotingSystem.model.Vote;
+import ru.kuznetsova.topjava.lunchVotingSystem.service.RatingService;
+import ru.kuznetsova.topjava.lunchVotingSystem.web.AbstractControllerTest;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.kuznetsova.topjava.lunchVotingSystem.RatingTestData.*;
+import static ru.kuznetsova.topjava.lunchVotingSystem.RestaurantTestData.*;
+import static ru.kuznetsova.topjava.lunchVotingSystem.UserTestData.USER_ID;
+
+class AdminRatingRestControllerTest extends AbstractControllerTest {
+
+    private static final String REST_URL = AdminRatingRestController.REST_URL + '/';
+
+    @Autowired
+    private RatingService ratingService;
+
+    @Test
+    void getRatingForDate() throws Exception {
+        mockMvc.perform(get(REST_URL + "all/" + MAY_30_2015))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(getRatingMatcher( RATING_R1_D30, RATING_R2_D30, RATING_R3_D30));
+    }
+
+    @Test
+    void getAllVotes() throws Exception {
+        TestUtil.print(mockMvc.perform(get(REST_URL + "all"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(getVoteMatcher(VOTE_U1_D30, VOTE_U2_D30, VOTE_U3_D30, VOTE_U4_D30, VOTE_U5_D30, VOTE_U2_D31,
+                VOTE_U3_D31, VOTE_U4_D31, VOTE_U5_D31)));
+    }
+
+    @Test
+    void getRatingForRestaurantName() throws Exception {
+        int id = RESTAURANT_ID + 1;
+        mockMvc.perform(get(REST_URL + id))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(getRatingMatcher(RATING_R2_D31, RATING_R2_D30));
+    }
+
+    @Test
+    void getRatingForRestaurantForDate() throws Exception {
+        mockMvc.perform(get(REST_URL + RESTAURANT_ID + "/" + MAY_30_2015))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(getRatingMatcher(RATING_R1_D30));
+    }
+
+    @Test
+    void getVoteForUserForDate() throws Exception {
+        mockMvc.perform(get(REST_URL + "user/" + USER_ID + "/" + MAY_30_2015))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(getVoteMatcher(VOTE_U1_D30));
+    }
+
+    @Test
+    void deleteOldVotes() throws Exception {
+        mockMvc.perform(delete(REST_URL))
+            .andDo(print())
+            .andExpect(status().isNoContent());
+
+        List<Vote> voteList = ratingService.getAllVotes();
+        Assertions.assertEquals(0, voteList.size());
+    }
+
+    @Test
+    void deleteOldVotesForDate() throws Exception {
+        mockMvc.perform(delete(REST_URL + MAY_31_2015))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+
+        List<Vote> voteList = ratingService.getAllVotes();
+        assertMatchVotes(voteList, VOTE_U2_D31, VOTE_U3_D31, VOTE_U4_D31, VOTE_U5_D31);
+    }
+
+}
